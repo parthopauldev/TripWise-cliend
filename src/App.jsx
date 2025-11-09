bf0366ef7c96071f666c0a816089b8a7f8bc9701
parthopauldev <parthopaul.dev@gmail.com>
2025-11-09 08:13:55 -0600

    files initialize

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     
+      <h1 className='bg-amber-300 text-primary'>Vite + React</h1>
+      <button className='my-custom-btn'>ghththt</button>
+     
+    </>
+  )
+}
+
+export default App
