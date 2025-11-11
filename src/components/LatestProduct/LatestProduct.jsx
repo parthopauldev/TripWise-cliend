4f00592148130ab0e3f5cec67d17797cca20c6a3
parthopauldev <parthopaul.dev@gmail.com>
2025-11-10 21:33:38 -0600

    All Prodacts loaded on All Vehicles page

--- src/components/LatestProduct/LatestProduct.jsx
+++ src/components/LatestProduct/LatestProduct.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router';
 
-const LatestProduct = ({ product}) => {
-    const {vehicleName}=product
+const LatestProduct = ({ product }) => {
+   
+    
+    const {vehicleName,_id}=product
     return (
         <div>
+
             <p>{vehicleName}</p>
+            <Link to={`/productDetails/${_id}`}>Details</Link>
         </div>
     );
 };
--- src/pages/AllVehicles/AllVehicles.jsx
+++ src/pages/AllVehicles/AllVehicles.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useAxios from '../../hooks/useAxios';
+import LatestProduct from '../../components/LatestProduct/LatestProduct';
 
 const AllVehicles = () => {
-    return (
-        <div>
-            this is AllVehicles
-        </div>
-    );
+    const axiosInstance = useAxios();
+    const [allProducts, setAllProducts] = useState([]); 
+  const [loading, setLoading] = useState(true);
+   
+  axiosInstance.get("/products").then((data) => {
+      const allProducts = data.data;
+     console.log(allProducts);
+     
+      setAllProducts(allProducts); // 4. Update the state
+        setLoading(false);
+    
+  });
+    if (loading) {
+        return <div>loading ...</div>
+    }
+     return <div>
+      
+        {
+        allProducts.map(product => (
+                <LatestProduct product={product}></LatestProduct>
+            ))
+        }
+  </div>;
 };
 
 export default AllVehicles;
